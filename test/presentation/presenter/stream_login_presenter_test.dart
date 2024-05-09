553faf77d5fe4d805488f4c5baa8f53c3da4e462
Carlos <carloslage.dev@gmail.com>
2024-05-08 23:27:03 -0300

    move StreamLoginPresenter to its own file

--- test/presentation/presenter/stream_login_presenter_test.dart
+++ test/presentation/presenter/stream_login_presenter_test.dart
@@ -1,38 +1,14 @@
 import 'dart:async';
 
 import 'package:faker/faker.dart';
+import 'package:fordevs/presentation/presenters/presenters.dart';
+import 'package:fordevs/presentation/protocols/protocols.dart';
 import 'package:mockito/annotations.dart';
 import 'package:mockito/mockito.dart';
 import 'package:test/test.dart';
 
 @GenerateNiceMocks([MockSpec<Validation>()])
 import 'stream_login_presenter_test.mocks.dart';
-
-abstract class Validation {
-  validate({required String field, required String value});
-}
-
-class LoginState {
-  String? emailError;
-}
-
-class StreamLoginPresenter {
-  final Validation validation;
-
-  final _controller = StreamController<LoginState>.broadcast();
-
-  final _state = LoginState();
-
-  Stream<String?>? get emailErrorStream =>
-      _controller.stream.map((state) => state.emailError);
-
-  StreamLoginPresenter({required this.validation});
-
-  void validateEmail(String email) {
-    _state.emailError = validation.validate(field: 'email', value: email);
-    _controller.add(_state);
-  }
-}
 
 void main() {
   late StreamLoginPresenter sut;
