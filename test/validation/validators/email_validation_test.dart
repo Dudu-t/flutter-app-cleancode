f00240b33b79ea40142c3630975c1d78240df0fe
Carlos <carloslage.dev@gmail.com>
2024-05-11 01:50:03 -0300

    ensure emailvalidation returns null if email is empty

---
+++ test/validation/validators/email_validation_test.dart
@@ -0,0 +1,28 @@
+import 'dart:math';
+
+import 'package:fordevs/validation/protocols/field_validation.dart';
+import 'package:test/test.dart';
+
+void main() {
+  late EmailValidation sut;
+  setUp(() {
+    sut = EmailValidation(field: 'any_field');
+  });
+  test('Should return null if email is empty', () {
+    expect(sut.validate(''), null);
+  });
+  test('Should return null if email is null', () {
+    expect(sut.validate(null), null);
+  });
+}
+
+class EmailValidation implements FieldValidation {
+  final String field;
+
+  EmailValidation({required this.field});
+
+  @override
+  String? validate(String? value) {
+    return null;
+  }
+}
